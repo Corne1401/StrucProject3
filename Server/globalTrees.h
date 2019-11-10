b5d8708b55474b2fe5b2cf4f181bb2425213afa6
Daniel Cornejo <danicor14@hotamail.com>
2019-11-10 16:24:45 -0600

    prueba arbol global

---
+++ Server/globalTrees.h
@@ -0,0 +1,8 @@
+#ifndef GLOBALTREES_H
+#define GLOBALTREES_H
+
+#include "TreesHandlers/BinarySearchTree.h"
+
+extern BinarySearchTree aisles;
+
+#endif // GLOBALTREES_H
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,9 +1,15 @@
 #include <QCoreApplication>
 #include "server.h"
+#include "globalTrees.h"
+#include "helpers/helpers.h"
+
+BinarySearchTree aisles;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    initAisleTree(aisles);
 
     Server Server;
     Server.StartServer();
